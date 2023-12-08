8b5ef6e2d74c80197b4113df2b8cbc9bf31c1357
Amolak Singh <samolak67@gmail.com>
2023-12-08 17:15:56 +0530

    easy done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,49 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const result = [];
+
+  transactions.forEach((tran) => {
+    const category = tran.category;
+    const price = tran.price;
+
+    const existingCategory = result.find((item) => item.category === category);
+
+    if (existingCategory) {
+      existingCategory.totalSpent += price;
+    } else {
+      result.push({
+        category,
+        totalSpent: price,
+      });
+    }
+  });
+  return result;
 }
 
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656105600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656134400000,
+    price: 30,
+    category: "Food",
+    itemName: "Sushi",
+  },
+];
+
+console.log(calculateTotalSpentByCategory(transactions));
 module.exports = calculateTotalSpentByCategory;
