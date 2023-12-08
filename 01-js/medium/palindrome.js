4a944fb0d22dd05894449be531630cbfd983380a
Amolak Singh <samolak67@gmail.com>
2023-12-08 17:11:46 +0530

    easy and medium done

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,14 @@
 */
 
 function isPalindrome(str) {
+  str = str.replace(/ /g, "");
+  str = str.replace(/[,.!?]/g, "");
+  str = str.toLowerCase();
+
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) return false;
+  }
   return true;
 }
 
